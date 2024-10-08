8c4f41a1d4bcba57227f0c44b9244225bb14bc66
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-10-08 09:58:48 +0530

    Time: 34 ms (100.00%) | Memory: 32.5 MB (98.20%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minSwaps(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
         int unbal=0,swp=0;
         for(char str:s){
             if(str==']'){
