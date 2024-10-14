ecbbde19c4ec6ffaeb7adfe86a477f26b4684255
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-10-14 09:33:04 +0530

    Time: 133 ms (98.75%) | Memory: 75.8 MB (96.76%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        long long res=0;
+        priority_queue <int> pq(nums.begin(),nums.end());
+        while(k!=0){
+            int ele=pq.top();pq.pop();
+            res+=ele;
+            ele=ceil((ele+2)/3);k--;
+            pq.push(ele);
+        }
+        return res;
+    }
+};
