7ccb5bac8238f64c45d308caebe77bbb7c0e180a
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-11-09 14:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (82.17%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     long minEnd(int n, int x) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
         long result = x;
         long remaining = n - 1;
         long position = 1;
