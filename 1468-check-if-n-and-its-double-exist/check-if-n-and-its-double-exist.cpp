99e720df7352b14e494904d74652531ed5ae5898
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-01 23:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (14.17%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -3,13 +3,13 @@ public:
     bool checkIfExist(vector<int>& arr) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
-        unordered_map<int,int> hmap;
+        unordered_set<int> hmap;
         for(int i=0;i<arr.size();i++){
             int x=arr[i]*2,y=arr[i]/2;
-            if((hmap.find(x)!=hmap.end())||((arr[i]%2==0)&&(hmap.find(y)!=hmap.end()))){
+            if((hmap.count(x))||((arr[i]%2==0)&&(hmap.count(y)))){
                 return true;
             }
-            hmap[arr[i]]=i;
+            hmap.insert(arr[i]);
         }
         return false;
     }
