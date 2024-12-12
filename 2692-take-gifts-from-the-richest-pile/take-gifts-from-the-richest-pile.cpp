b1c7d8a68959a636b3b165b263a6620ae818dd3f
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-12 20:09:41 +0530

    Time: 2 ms (61.64%) | Memory: 13.5 MB (7.74%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        make_heap(gifts.begin(), gifts.end());
-        for (int i=0, x=INT_MAX; i<k && x>1; i++){
-            pop_heap(gifts.begin(), gifts.end());
-            x=gifts.back();
-            gifts.back()=sqrt(gifts.back());
-            push_heap(gifts.begin(), gifts.end());
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        priority_queue<int> pq(gifts.begin(),gifts.end());
+        long long ans=0;
+        while(k--){
+            int num=pq.top();
+            pq.pop();
+            pq.push(sqrt(num));
         }
-        return accumulate(gifts.begin(), gifts.end(), 0LL);
+        while(!pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+        }
+        return ans;
     }
 };
