c4e545f356937adec2d3b99305f45d6349023de6
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-16 10:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (8.33%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        if(num==0){return true;}
+        if(num%10==0){return false;}
+        else{return true;}
+    }
+};
