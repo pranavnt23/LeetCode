5c0c1f1aaeeb8f963eae08e252b979cc5d6885a8
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-16 12:09:59 +0530

    Time: 3 ms (45.26%) | Memory: 7.9 MB (9.72%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-    /*    ios_base::sync_with_stdio(0);
+        ios_base::sync_with_stdio(0);
         cin.tie(0);cout.tie(0);
-        int num=0;
+    /*    int num=0;
         while(n>1){
             if(n%2==0){n=n/2;num+=n;}
             else{n=(n/2)+1;num+=(n-1);}
