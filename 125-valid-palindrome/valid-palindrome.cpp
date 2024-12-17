363511e334d0d8f02fbc239c51d5a5d58087e3c0
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-17 15:26:17 +0530

    Time: 8 ms (7.28%) | Memory: 10.4 MB (6.61%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string s1="";
+        for(auto str:s){
+            if(!iswalnum(str)){continue;}
+            if(isupper(str)){
+                s1+=tolower(str);
+            }
+            else{s1+=str;}
+        }
+        //if(s1.size()==1){return false;}
+        int i=0,j=s1.size()-1;
+        while(i<j){
+            if(s1[i]!=s1[j]){return false;}
+            i++;j--;
+        }
+        return true;
+    }
+};
