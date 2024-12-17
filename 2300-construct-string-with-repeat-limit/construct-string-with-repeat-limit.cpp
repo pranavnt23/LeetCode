8963902687cb728abfdc235957a5431ea9b8e5fc
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-17 10:18:15 +0530

    Time: 47 ms (49.62%) | Memory: 28.4 MB (21.08%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int rep) {
+        string ans = "";
+        unordered_map<char, int> hmap;
+        priority_queue<pair<char, int>> pq;
+        for (auto atr : s) {hmap[atr]++;}
+        for (auto [c, cnt] : hmap) {pq.push({c, cnt});}
+        while (!pq.empty()) {
+            auto [ch, num] = pq.top(); 
+            pq.pop();
+
+            int use = min(rep, num);
+            ans.append(use, ch);
+            num -= use;
+            if (num > 0) {
+                if (pq.empty()) break;
+                auto [ch2, num2] = pq.top();
+                pq.pop();
+                
+                ans += ch2; 
+                num2--;
+                if (num > 0) pq.push({ch, num});
+                if (num2 > 0) pq.push({ch2, num2});
+            }
+        }
+        return ans;
+    }
+};
