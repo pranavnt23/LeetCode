03652c5722739194b66194a3b159b99b6517a517
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-18 20:25:54 +0530

    Time: 11 ms (15.88%) | Memory: 14.8 MB (10.08%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int n[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string s[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        //int n[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        //string s[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        unordered_map<int,string> hmap;
+        hmap[1000]="M";hmap[900]="CM";
+        hmap[500]="D";hmap[400]="CD";
+        hmap[100]="C";hmap[90]="XC";
+        hmap[50]="L";hmap[40]="XL";
+        hmap[10]="X";hmap[9]="IX";
+        hmap[5]="V";hmap[4]="IV";hmap[1]='I';
 
-        int i = 0;
+        vector<int> keys = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        int i=0;string str;
+        for(auto key:keys){
+            while(num>=key){
+                str+=hmap[key];
+                num-=key;
+            }
+        }
+
+        /*int i = 0;
         string str; 
         while (num > 0) {
             if (num >= n[i]) {
@@ -13,7 +30,7 @@ public:
             } else {
                 i++;
             }
-        }
+        }*/
         return str;  
     }
 };
