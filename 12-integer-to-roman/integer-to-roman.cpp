46700f983b209d230b362b6f9562aed74df3ce2a
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-18 20:27:03 +0530

    Time: 18 ms (6.26%) | Memory: 14.7 MB (10.30%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        //int n[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        //string s[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         unordered_map<int,string> hmap;
         hmap[1000]="M";hmap[900]="CM";
         hmap[500]="D";hmap[400]="CD";
@@ -20,17 +18,6 @@ public:
                 num-=key;
             }
         }
-
-        /*int i = 0;
-        string str; 
-        while (num > 0) {
-            if (num >= n[i]) {
-                str = str + s[i];
-                num -= n[i];
-            } else {
-                i++;
-            }
-        }*/
         return str;  
     }
 };
