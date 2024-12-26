affbd956c8e851706c59381a502f70c8a43572fa
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2024-12-26 11:51:40 +0530

    Time: 953 ms (8.11%) | Memory: 11.6 MB (68.89%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,31 +1,16 @@
 class Solution {
-    vector<char> ops = {'+', '-'};
-
-    void findWays(const vector<int>& nums, vector<bool>& taken, int target, int &count, int curr = 0, int idx = 0) {
-        if (idx == nums.size() and curr != target) return;
+    int findWays(const vector<int>& nums, int target, int curr = 0, int idx = 0) {
+        if (idx == nums.size() and curr != target)
+	        return 0;
         if (curr == target and idx == nums.size()) {
-            count++;
-            return;
+            return 1;   
         }
 
-        for (auto op : ops) {
-            if (!taken[idx]) {
-                taken[idx] = true;
-                if (op == '+')
-                    findWays(nums, taken, target, count, curr + nums[idx], idx + 1);
-                else
-                    findWays(nums, taken, target, count, curr - nums[idx], idx + 1);
-                taken[idx] = false;
-            }
-        }
+	return  findWays(nums, target, curr + nums[idx], idx + 1)
+	       +findWays(nums, target, curr - nums[idx], idx + 1);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int count = 0;
-        vector<bool> taken(nums.size());
-
-        findWays(nums, taken, target, count);
-
-        return count;
+        return findWays(nums, target);
     }
 };
