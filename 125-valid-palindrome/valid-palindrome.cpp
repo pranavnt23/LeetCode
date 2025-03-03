68b05401f9ca3f5ea8c9c3dbc9a90ab30b0fa77e
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2025-03-03 22:22:25 +0530

    Time: 1 ms (48.88%) | Memory: 10.1 MB (41.86%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -9,7 +9,6 @@ public:
             }
             else{s1+=str;}
         }
-        //if(s1.size()==1){return false;}
         int i=0,j=s1.size()-1;
         while(i<j){
             if(s1[i]!=s1[j]){return false;}
