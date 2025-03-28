2dc8c4e9660e28042c33beb5d982fc241d14eae7
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2025-03-28 22:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.40%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3) return n;
+        int prev1=3,prev2=2,curr=0;
+        for(int i=3;i<n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;prev1=curr;
+        }
+        return curr;
+    }
+};
