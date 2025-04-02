4d5b499b722c89b4140b187643051e16bcdaadbb
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2025-04-02 09:55:06 +0530

    Time: 31 ms (30.38%) | Memory: 111.9 MB (7.01%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -4,10 +4,7 @@ public:
         stack<pair<int, int>> stk;
 
         vector<int> ans(temperatures.size(), 0);
-
-        stk.push({0, temperatures[0]});
-
-        for (int i = 1; i < temperatures.size(); i++) {
+        for (int i = 0; i < temperatures.size(); i++) {
             while (!stk.empty() and temperatures[i] > stk.top().second) {
                 ans[stk.top().first] = (i - stk.top().first);
                 stk.pop();
