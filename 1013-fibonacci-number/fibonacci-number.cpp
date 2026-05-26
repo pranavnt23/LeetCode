98538e1399e45c7479810182bc8d836869aef025
pranavnt23 <123755545+pranavnt23@users.noreply.github.com>
2026-05-26 10:21:47 +0530

    Time: 2 ms (76.83%) | Memory: 8 MB (22.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        int fib=0,prev=0,curr=1;
-        if(n==1) return 1;
-        for(int i=1;i<n;i++){
-            fib=0;
-            fib=prev+curr;
-            prev=curr;
-            curr=fib;
+        vector<int> fib;
+        if(n==1||n==0) return n;
+        fib.push_back(0);
+        fib.push_back(1);
+        for(int i=2;i<=n;i++){
+            fib.push_back(fib[i-1]+fib[i-2]);
         }
-        return fib;
+        return fib[n];
     }
 };
