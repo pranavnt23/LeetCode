35745a33295afbce2ff81bcdfa3ad2711e435286
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 17:56:53 +0530

    Time: 22 ms (32.26%) | Memory: 25 MB (67.86%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> strs1;vector<vector<string>> res;
-        for (auto str : strs) {
-            sort(str.begin(), str.end()); 
-            strs1.push_back(str); 
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);cout.tie(NULL);
+        unordered_map<string,vector<string>> hmap;
+        for(string s:strs){
+            string key=s;
+            sort(key.begin(),key.end());
+            hmap[key].push_back(s);
         }
-
-        vector<bool> visited(strs.size(), false);
-        
-        for (int i = 0; i < strs1.size(); i++) {
-            if (visited[i]) continue; 
-            vector<string> temp;
-            temp.push_back(strs[i]); 
-        
-            for (int j = i + 1; j < strs1.size(); j++) {
-                if (strs1[i] == strs1[j]) { 
-                    temp.push_back(strs[j]); 
-                    visited[j] = true; 
-                }
-            }
-            res.push_back(temp); 
+        vector<vector<string>> res;
+        for(const auto&[key,ans]:hmap){
+            res.push_back(ans);
         }
         return res;
     }
