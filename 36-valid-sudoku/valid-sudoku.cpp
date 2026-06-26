79b5ef2a453142af9e8c8891f165b9247d6fc2f9
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 21:17:08 +0530

    Time: 6 ms (34.52%) | Memory: 24.6 MB (34.68%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<board.size();i++){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        for(int i=0;i<9;i++){
             unordered_map<char,int> hmap;
-            for(int j=0;j<board[0].size();j++){
-                char ele=board[i][j];
-                if(ele!='.') hmap[ele]++;
-                if(hmap[ele]>1) return false;
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.') hmap[board[i][j]]++;
+                if(hmap[board[i][j]]>1) return false;
             }
         }
-        for(int i=0;i<board.size();i++){
+        for(int i=0;i<9;i++){
             unordered_map<char,int> hmap;
-            for(int j=0;j<board[0].size();j++){
-                char ele=board[j][i];
-                if(ele!='.') hmap[ele]++;
-                if(hmap[ele]>1) return false;
+            for(int j=0;j<9;j++){
+                if(board[j][i]!='.') hmap[board[j][i]]++;
+                if(hmap[board[j][i]]>1) return false;
             }
         }
-        for(int row = 0; row < 9; row += 3) {
-            for(int col = 0; col < 9; col += 3) {
-                unordered_map<char, int> hmap;
-                for(int i = 0; i < 3; i++) {
-                    for(int j = 0; j < 3; j++) {
-                        char ele = board[row + i][col + j];
-                        if(ele != '.') {
-                            hmap[ele]++;
-                            if(hmap[ele] > 1) return false;
-                        }
+        for(int row=0;row<9;row+=3){
+            for(int col=0;col<9;col+=3){
+                unordered_map<char,int> hmap;
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                        if(board[row+i][col+j]!='.') hmap[board[row+i][col+j]]++;
+                        if(hmap[board[row+i][col+j]]>1) return false;
                     }
                 }
             }
