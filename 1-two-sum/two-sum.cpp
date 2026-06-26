448281e7626ab586390a6a73523a6cfa262c159e
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 17:16:32 +0530

    Time: 1 ms (76.57%) | Memory: 14.8 MB (37.18%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,10 +4,11 @@ public:
         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         unordered_map<int,int> hmap;
         for(int i=0;i<nums.size();i++){
-            if(hmap.find(target-nums[i])!=hmap.end()) {
-                return{i,hmap[target-nums[i]]};
+            int num=nums[i];
+            if(hmap.find(target-num)!=hmap.end()) {
+                return{i,hmap[target-num]};
             }
-            hmap[nums[i]]=i;
+            hmap[num]=i;
         }
         return{};
     }
