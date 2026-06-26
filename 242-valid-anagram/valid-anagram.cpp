72a8722d964f42a33d6dd3e825cc4befb29ad898
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 16:32:51 +0530

    Time: 5 ms (26.27%) | Memory: 9.9 MB (11.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,12 +4,10 @@ public:
         if(str.size()!=ttr.size()) return false;
         unordered_map<char,int> hmap;
         for(int i=0;i<str.size();i++){
-            char s=str[i],t=ttr[i];
-            hmap[s]++;hmap[t]--;
+            hmap[str[i]]++;hmap[ttr[i]]--;
         }
-        for(int i=0;i<str.size();i++){
-            char s=str[i],t=ttr[i];
-            if(hmap[s]!=hmap[t]){return false;}
+        for(const auto&[charr,mapp]:hmap){
+            if(mapp!=0) return false;
         }
         return true;
     }
