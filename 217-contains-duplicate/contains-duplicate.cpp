7f7e9f9842b9a311d323f8fdcb69fe19c1c19b56
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 16:42:32 +0530

    Time: 52 ms (42.19%) | Memory: 90.8 MB (45.85%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(NULL);
         unordered_map<int, int> hmap;
         for(int i=0;i<nums.size();i++){
             hmap[nums[i]]++;
