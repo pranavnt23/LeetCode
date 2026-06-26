ea362e27a3ea1418f64bde9ab977119390aac31f
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-06-26 16:34:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string str, string ttr) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(str.size()!=ttr.size()) return false;
         unordered_map<char,int> hmap;
         for(int i=0;i<str.size();i++){
