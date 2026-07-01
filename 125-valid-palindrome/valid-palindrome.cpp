ca777a28712e0ad536f67831415597ac56d71ed2
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-07-01 20:11:47 +0530

    Time: 1 ms (43.87%) | Memory: 10.3 MB (25.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string s1="";
-        for(auto str:s){
-            if(!iswalnum(str)){continue;}
-            if(isupper(str)){
-                s1+=tolower(str);
-            }
-            else{s1+=str;}
+        for(int i=0;i<s.size();i++){
+            char c=s[i];
+            if(!isalnum(c)) continue;
+            s1+=tolower(c);
         }
-        int i=0,j=s1.size()-1;
-        while(i<j){
-            if(s1[i]!=s1[j]){return false;}
+        int i=0;int j=s1.size()-1;
+        while(i<=j){
+            if(s1[i]!=s1[j]) return false;
             i++;j--;
         }
-        return true;
+        return true;    
     }
 };
