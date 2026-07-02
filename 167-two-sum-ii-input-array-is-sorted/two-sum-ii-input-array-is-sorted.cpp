7651f51b5ef3902047d91ee8c4574492845ec696
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-07-02 13:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (33.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> hmap;//<ele,ind>
-        vector<int> arr;
-        for(int i=0;i<nums.size();i++){
-            int ele=target-nums[i];
-            if(hmap[ele]!=0){
-                arr.push_back(hmap[ele]);
-                arr.push_back(i+1);
-                return arr;
-            }
-            else{
-                int elee=nums[i];
-                hmap[elee]=i+1;
-            }
+        int i=0,j=nums.size()-1;
+        vector<int> ans;
+        while(i<j){
+            if(nums[i]+nums[j]>target) j--;
+            if(nums[i]+nums[j]<target) i++;
+            if(nums[i]+nums[j]==target) return {i+1,j+1};
         }
-        return arr;
+        return {};
     }
 };
