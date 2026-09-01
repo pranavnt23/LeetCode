48f02d4019e7275f71fce2db4ceaf3eb2eaabcfc
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-09-01 20:03:27 +0530

    Time: 81 ms (5.10%) | Memory: 29.5 MB (5.37%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-
         set<pair<int,int>> visited;
-        vector<pair<int,int>> directions={{0,-1},{0,1},{-1,0},{1,0}};
         int row=grid.size(),col=grid[0].size();
+        vector<pair<int,int>> directions={{0,1},{0,-1},{1,0},{-1,0}};
         int island=0;
 
-        auto bfs = [&](int r, int c) {
-            queue<pair<int,int>> bfqueue;
-            visited.insert({r,c});
-            bfqueue.push({r,c});
+        auto bfs=[&](int i,int j){
+            queue<pair<int,int>> bfs;
+            bfs.push({i,j});visited.insert({i,j});
 
-            while(!bfqueue.empty()){
-                auto [r,c] =bfqueue.front();bfqueue.pop();
+            while(!bfs.empty()){
+                auto [r,c]=bfs.front();bfs.pop();
                 for(auto [dr,dc]:directions){
                     int nr=r+dr,nc=c+dc;
-                    if ((nr >= 0 && nr < row) && (nc >= 0 && nc < col) &&
-                        grid[nr][nc] == '1' &&
-                        visited.find({nr, nc}) == visited.end()){
-                            bfqueue.push({nr,nc});
-                            visited.insert({nr,nc});
+                    if((nr>=0 && nr<row) && (nc>=0 && nc<col) &&
+                    visited.find({nr,nc})==visited.end() && grid[nr][nc]=='1'){
+                        visited.insert({nr,nc});
+                        bfs.push({nr,nc});
                     }
                 }
             }
@@ -28,12 +25,13 @@ public:
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j]=='1'&& visited.find({i,j})==visited.end()){
+                if(grid[i][j]=='1' && visited.find({i,j})==visited.end()){
                     bfs(i,j);
                     island++;
                 }
             }
         }
+
         return island;
     }
 };
