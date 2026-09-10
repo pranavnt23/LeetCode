d9b83b9cfdd9727ab155775c93158ca759ad49a6
Pranav N T <123755545+pranavnt23@users.noreply.github.com>
2026-09-10 11:30:16 +0530

    Time: 3 ms (91.06%) | Memory: 15.8 MB (29.76%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    using int2=pair<int,int>;
+public:
+    int count=0;
+    int2 inOrder(TreeNode* node){
+        if(node==NULL) return {0,0};
+        auto [sumL,iL]=inOrder(node->left);
+        int sum=node->val,i=1;
+        auto [sumR,iR]=inOrder(node->right);
+        sum+=(sumL+sumR);
+        i+=(iL+iR);
+        if(sum/i==node->val) count++;
+        return {sum,i};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        inOrder(root);
+        return count;
+    }
+};
